d8581e17c95a68e53720725db46116d8581a409d
murali-mm <75167970+murali-mm@users.noreply.github.com>
2026-01-19 12:46:20 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "rgname" {
+    type = string
+}
+variable "rglocation" {
+    type = string
+    default = "CentalIndia"
+}
+variable "nic" {
+    type = string
+}
+variable "nsg" {
+    type = string
+}
+variable "prefix" {
+    type = string
+}
+variable "subnet" {
+    type = string
+}
+variable "vnet_cidr_prefix" {
+    type = string
+}
+variable "subnet_cidr_prefix" {
+    type = string
+}
