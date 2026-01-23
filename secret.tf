e10703de5487b9150a9c75ccfe43a60b8d4e0845
murali-mm <75167970+murali-mm@users.noreply.github.com>
2026-01-23 14:04:41 +0530

    Create secret.tf

---
+++ secret.tf
@@ -0,0 +1,5 @@
+
+client_id = "55f5a120-b536-40aa-b9b1-88bb8a101048"
+tenant_id = "518b32b2-b122-43e7-a766-960beb7499f3"
+client_secret = "D2A8Q~tCnTiQytGEyg6jPgXIgwgoXeiFh1Cntbge"
+subscription_id = "9f1d9528-b481-43e9-9ac6-aa5ee102f2ff"
